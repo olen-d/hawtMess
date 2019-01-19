4344ec1994f7e826476d2583898b90dc256f1f02
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-18 22:29:49 -0500

    l

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -149,7 +149,7 @@ const favs = {
         db.ref(`/favTweet/${userId}`).orderByChild("dateAdded").on("child_added", function(ss)  {
             let sv = ss.val();
             let sk = ss.ref.key;
-            console.log("11111111 " + sk);
+            console.log("-+-+-+-+-+-+ " + sv.sk.url);
             //console.log("---GREAT ++++===++++ SUCCeSS--- " + sv.prefChildKey.url);
             
             //let tweetURL = ss.val().url;
